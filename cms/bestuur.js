52ddafcd59939706e721759c349af13dfed56b89
Robin Janssens <robin@robinjanssens.net>
2017-06-28 00:29:13 +0200

    made color inverting on bestuurs pagina a boolean

--- cms/bestuur.js
+++ cms/bestuur.js
@@ -1,85 +1,89 @@
 {
-    cover_title: 'Bestuur | LVSV Antwerpen',
-    cover_subtitle: 'Providing essential liberty since 1947',
-    $header_image_type: 'image',
-    header_image: 'assets/img/bestuur.jpg',
-    members: [
-        {
-            $image_type: 'image',
-            image: 'assets/img/bestuur/edo.jpg',
-            name: 'Edo Schoone',
-            function: 'Voorzitter',
-            email: 'voorzitter@lvsvantwerpen.be',
-            email2: 'edo@lvsvantwerpen.be'
-        },
-        {
-            inverted_color: 'true',
-            $image_type: 'image',
-            image: 'assets/img/bestuur/robin.jpg',
-            name: 'Robin Janssens',
-            function: 'Ondervoorzitter en Secretaris',
-            email: 'robin@lvsvantwerpen.be',
-            email2: ''
-        },
-        {
-            $image_type: 'image',
-            image: 'assets/img/bestuur/jeroen.jpg',
-            name: 'Jeroen Van den Poel',
-            function: 'Penningmeester en Public Relations',
-            email: 'jeroen@lvsvantwerpen.be',
-            email2: ''
-        },
-        {
-            inverted_color: 'true',
-            $image_type: 'image',
-            image: 'assets/img/bestuur/igor.jpg',
-            name: 'Igor Zivkovic',
-            function: 'Politiek Secretaris',
-            email: 'igor@lvsvantwerpen.be',
-            email2: ''
-        },
-        {
-            $image_type: 'image',
-            image: 'assets/img/bestuur/joeri.jpg',
-            name: 'Joeri Vandendriessche',
-            function: 'Politiek Secretaris',
-            email: 'joeri@lvsvantwerpen.be',
-            email2: ''
-        },
-        {
-            inverted_color: 'true',
-            $image_type: 'image',
-            image: 'assets/img/bestuur/krystian.jpg',
-            name: 'Krystian Timperman',
-            function: 'Lustrumverantwoordelijke',
-            email: 'krystian@lvsvantwerpen.be',
-            email2: ''
-        },
-        {
-            $image_type: 'image',
-            image: 'assets/img/bestuur/laurenz.jpg',
-            name: 'Laurenz van Ginneken',
-            function: 'Prosenior',
-            email: 'laurenz@lvsvantwerpen.be',
-            email2: ''
-        },
-        {
-            inverted_color: 'true',
-            $image_type: 'image',
-            image: 'assets/img/bestuur/brent.jpg',
-            name: 'Brent Apers',
-            function: 'Aspirant',
-            email: 'brent@lvsvantwerpen.be',
-            email2: ''
-        }
-    ],
-    contact_title: 'Vind ons!',
-    facbook_url: 'https://www.facebook.com/LVSV.Antwerpen/',
-    twitter_url: 'https://twitter.com/LVSVAntwerpen',
-    google_url: '',
-    instagram_url: 'https://www.instagram.com/lvsv.antwerpen/',
-    linkedin_url: 'https://www.linkedin.com/company/10833720',
-    youtube_url: '',
-    rss_url: '',
-    email: 'voorzitter@lvsvantwerpen.be'
+	cover_title: 'Bestuur | LVSV Antwerpen',
+	cover_subtitle: 'Providing essential liberty since 1947',
+	$header_image_type: 'image',
+	header_image: 'assets/img/bestuur.jpg',
+	members: [
+		{
+			inverted_color: false,
+			$image_type: 'image',
+			image: 'assets/img/bestuur/edo.jpg',
+			name: 'Edo Schoone',
+			function: 'Voorzitter',
+			email: 'voorzitter@lvsvantwerpen.be',
+			email2: 'edo@lvsvantwerpen.be'
+		},
+		{
+			inverted_color: true,
+			$image_type: 'image',
+			image: 'assets/img/bestuur/robin.jpg',
+			name: 'Robin Janssens',
+			function: 'Ondervoorzitter en Secretaris',
+			email: 'robin@lvsvantwerpen.be',
+			email2: ''
+		},
+		{
+			inverted_color: false,
+			$image_type: 'image',
+			image: 'assets/img/bestuur/jeroen.jpg',
+			name: 'Jeroen Van den Poel',
+			function: 'Penningmeester en Public Relations',
+			email: 'jeroen@lvsvantwerpen.be',
+			email2: ''
+		},
+		{
+			inverted_color: true,
+			$image_type: 'image',
+			image: 'assets/img/bestuur/igor.jpg',
+			name: 'Igor Zivkovic',
+			function: 'Politiek Secretaris',
+			email: 'igor@lvsvantwerpen.be',
+			email2: ''
+		},
+		{
+			inverted_color: false,
+			$image_type: 'image',
+			image: 'assets/img/bestuur/joeri.jpg',
+			name: 'Joeri Vandendriessche',
+			function: 'Politiek Secretaris',
+			email: 'joeri@lvsvantwerpen.be',
+			email2: ''
+		},
+		{
+			inverted_color: true,
+			$image_type: 'image',
+			image: 'assets/img/bestuur/krystian.jpg',
+			name: 'Krystian Timperman',
+			function: 'Lustrumverantwoordelijke',
+			email: 'krystian@lvsvantwerpen.be',
+			email2: ''
+		},
+		{
+			inverted_color: false,
+			$image_type: 'image',
+			image: 'assets/img/bestuur/laurenz.jpg',
+			name: 'Laurenz van Ginneken',
+			function: 'Prosenior',
+			email: 'laurenz@lvsvantwerpen.be',
+			email2: ''
+		},
+		{
+			inverted_color: true,
+			$image_type: 'image',
+			image: 'assets/img/bestuur/brent.jpg',
+			name: 'Brent Apers',
+			function: 'Aspirant',
+			email: 'brent@lvsvantwerpen.be',
+			email2: ''
+		}
+	],
+	contact_title: 'Vind ons!',
+	facbook_url: 'https://www.facebook.com/LVSV.Antwerpen/',
+	twitter_url: 'https://twitter.com/LVSVAntwerpen',
+	google_url: '',
+	instagram_url: 'https://www.instagram.com/lvsv.antwerpen/',
+	linkedin_url: 'https://www.linkedin.com/company/10833720',
+	youtube_url: '',
+	rss_url: '',
+	email: 'voorzitter@lvsvantwerpen.be'
 }
