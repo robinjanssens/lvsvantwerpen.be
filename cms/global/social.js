2bc3b25e2c28236422debdb17932151efd3ee011
Robin Janssens <robin@robinjanssens.net>
2017-07-10 17:07:35 +0200

    added social content

---
+++ cms/global/social.js
@@ -0,0 +1,11 @@
+{
+	contact_title: 'Vind ons!',
+	facbook_url: 'https://www.facebook.com/LVSV.Antwerpen/',
+ 	twitter_url: 'https://twitter.com/LVSVAntwerpen',
+ 	google_url: '',
+ 	instagram_url: 'https://www.instagram.com/lvsv.antwerpen/',
+ 	linkedin_url: 'https://www.linkedin.com/company/10833720',
+	youtube_url: '',
+ 	rss_url: '',
+ 	email: 'voorzitter@lvsvantwerpen.be'
+}
